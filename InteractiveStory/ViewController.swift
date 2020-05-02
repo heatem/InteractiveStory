aa3f2c4685322fc2450da524ef29098a90f542fc
Heather Mason <heatem@gmail.com>
2020-05-02 00:02:34 -0400

    Creating the User Interface Programmatically

--- InteractiveStory/ViewController.swift
+++ InteractiveStory/ViewController.swift
@@ -13,5 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "startAdventure" {
+            guard let pageController = segue.destination as? PageController else { return }
+            
+            pageController.page = Adventure.story
+        }
+    }
 }
 
